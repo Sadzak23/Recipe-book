73e67d2337e32b151d5207d6eb2d98cc0bdedf06
Aleksandar Sadzak <sadzak23@gmail.com>
2020-03-23 21:07:48 +0100

    Create shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.modul';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
-  constructor() { }
+  ingredients: Ingredient[] = [
+    new Ingredient("Lemon", 4, "pcs"),
+    new Ingredient("Orange", 1.5, "kg")
+  ];
+  constructor() {
+  }
 
   ngOnInit() {
   }
